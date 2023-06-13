c943bd5be278bb816dcd797ea232adbab4409f7c
Vitor Samuel <108810741+VitorSmelo021@users.noreply.github.com>
2023-06-13 16:58:56 -0300

    Add files via upload

--- Recomposicao/index.js
+++ Recomposicao/index.js
@@ -1,42 +1,39 @@
-const profileImage = document.querySelector("#profile-img")
-const titulo = document.querySelector("#title")
-const nome = document.querySelector("#name")
-const informacao = document.querySelector("#info")
-const fotoSeguidor = document.querySelector("#follow-img")
-const seguidor = document.querySelector("#follow-name")
+const profileImage = document.querySelector("#profile-img");
+const titulo = document.querySelector("#title");
+const nome = document.querySelector("#name");
+const informacao = document.querySelector("#info");
+const fotoSeguidor = document.querySelector("#follow-img");
+const seguidor = document.querySelector("#follow-name");
 const seguidores = document.querySelector("#seguidores");
 
 window.addEventListener("DOMContentLoaded", () => {
-    fetchData()
-    fetchDato()
+  fetchData();
+  fetchDato();
 });
 
 async function fetchData() {
-    await fetch("https://api.github.com/users/VitorSmelo021")
+  await fetch("https://api.github.com/users/VitorSmelo021")
     .then((response) => response.json())
     .then((data) => {
-        profileImage.setAttribute("src", data.avatar_url)
-        titulo.innerHTML = data.company
-        nome.innerHTML = data.name
-        informacao.innerHTML = data.bio
+      profileImage.setAttribute("src", data.avatar_url);
+      titulo.innerHTML = data.company;
+      nome.innerHTML = data.name;
+      informacao.innerHTML = data.bio;
     });
 }
 async function fetchDato() {
-    await fetch("https://api.github.com/users/VitorSmelo021/followers")
+  await fetch("https://api.github.com/users/VitorSmelo021/followers")
     .then((response) => response.json())
     .then((seguir) => {
-      
-        seguir.map((item) => {
-            seguidores.innerHTML += `
+      seguir.map((item) => {
+        seguidores.innerHTML += `
             
             <div id ="caixa-seguidor">
             <img id="follow-img" src="${item.avatar_url}" alt="">
             <p id="follow-name">${item.login}</p>
             </div>
             
-            `
-        });
+            `;
+      });
     });
 }
-
-
